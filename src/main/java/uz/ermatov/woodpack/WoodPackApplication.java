377ca113b89492fb286bf3634fff48584c1a2993
Samir <sharipovsamir480@gmail.com>
2025-02-07 15:47:01 +0500

    first commit

---
+++ src/main/java/uz/ermatov/woodpack/WoodPackApplication.java
@@ -0,0 +1,13 @@
+package uz.ergashov.woodpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodPackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodPackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/ermatov/woodpack/WoodPackApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ergashov.woodpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodPackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
